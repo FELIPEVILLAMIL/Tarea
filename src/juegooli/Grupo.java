5d0ed618d52e4c6fca55bedb7a537ac9597d3cbf
Usuario14 <Usuario14@FI4531119-14>
2016-03-07 15:50:41 -0500

    c1

---
+++ src/juegooli/Grupo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegooli;
+
+/**
+ *
+ * @author Usuario14
+ */
+public class Grupo {
+   private String nDeporte;
+
+    public Grupo(String nDeporte) {
+        this.nDeporte = nDeporte;
+    }
+   
+}
